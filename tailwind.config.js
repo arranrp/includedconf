2e1363531947ec3568d53d0f26c4838ddefb9014
Arran Ross-Paterson <arran@welikepie.com>
2022-02-05 21:23:01 +0000

    tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'giga': ['"Lexend Giga"', 'sans-serif'],
+      'welcome': ['"Source Serif Pro"', 'serif'],
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+      'super-sized': '8rem',
+    }
+  },
+  plugins: [],
+}
