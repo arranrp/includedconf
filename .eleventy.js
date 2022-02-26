9545e8228886815c85e70beb4524d2ec4a1101c5
Arran Ross-Paterson <arran@welikepie.com>
2022-02-26 14:59:35 +0000

    twitter

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/images');
     eleventyConfig.addPassthroughCopy('src/site.webmanifest');
+    eleventyConfig.addPassthroughCopy('src/robot.txt');
     return {
         dir: {
             input: "src",
